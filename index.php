a2c578dc0a89784cc7d20e0cf7544c3ee6ce958e
FELIPE DE MELO FERNANDES <21192908@CFP127ALUNO.LOCAL>
2021-09-10 07:50:35 -0300

    Site funcionando

--- index.php
+++ index.php
@@ -11,26 +11,25 @@ if (isset($_GET["buscarFuncionario"])) {
 }
 
 if (
-    !empty($_POST["first_name"]) && !empty($_POST["last_name"]) &&
-    !empty($_POST["email"]) && !empty($_POST["gender"]) &&
-    !empty($_POST["ip_address"]) && !empty($_POST["country"])
-    && !empty($_POST["department"])
+    !empty($_GET["first_name"]) && !empty($_GET["last_name"]) &&
+    !empty($_GET["email"]) && !empty($_GET["gender"]) &&
+    !empty($_GET["ip_address"]) && !empty($_GET["country"])
+    && !empty($_GET["department"])
 ) {
     adicionarFuncionario([
-        "first_name" => $_POST["first_name"],
-        "last_name" => $_POST["last_name"],
-        "email" => $_POST["email"],
-        "gender" => $_POST["gender"],
-        "ip_address" => $_POST["ip_address"],
-        "country" => $_POST["country"],
-        "department" => $_POST["department"],
+        "first_name" => $_GET["first_name"],
+        "last_name" => $_GET["last_name"],
+        "email" => $_GET["email"],
+        "gender" => $_GET["gender"],
+        "ip_address" => $_GET["ip_address"],
+        "country" => $_GET["country"],
+        "department" => $_GET["department"],
     ]);
 
     header('location:' . dirname($_SERVER['PHP_SELF']));
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -93,14 +92,14 @@ if (
         <!-- <div id="bg"></div> -->
         <div class="modal">
             <h2>Adição de novo funcionário</h2>
-            <form action="acoes.php" method="POST">
+            <form>
                 <input type="text" name="first_name" required placeholder="Primeiro nome">
                 <input type="text" name="last_name" required placeholder="Último nome">
                 <input type="text" name="email" required placeholder="Email">
                 <select name="gender" id="gender" required placeholder="Sexo">
                     <option value="Male">Masculino</option>
                     <option value="Female">Feminino</option>
-                    <option value="Other">Outros</option>
+                    <option value="WarMachine">Máquina de Combate</option>
                 </select>
                 <input type="text" name="ip_address" required placeholder="Endereço IP">
                 <input type="text" name="country" required placeholder="País">
