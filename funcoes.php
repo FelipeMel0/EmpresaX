7ad7b1986e85ba26b14b529b92201f86b7007e7c
FELIPE DE MELO FERNANDES <21192908@CFP127ALUNO.LOCAL>
2021-09-23 11:26:19 -0300

    Tela de login

--- funcoes.php
+++ funcoes.php
@@ -82,3 +82,48 @@ function editarFuncionario($nomeArquivo, $funcionarioEditado)
 
     file_put_contents($nomeArquivo, $jsonArray);
 }
+
+function realizarLogin($usuario, $senha, $dados)
+{
+    foreach ($dados as $dado) {
+
+        if ($dado->usuario == $usuario && $dado->senha == $senha) {
+
+            //variáveis de sessão
+            $_SESSION["usuario"] = $dado->usuario;
+            $_SESSION["id"] = session_id();
+            $_SESSION["dataHora"] = date('d/m/Y - h:i:s');
+
+            header('location: index.php');
+            exit;
+        } 
+        else {
+
+            // header('location: index.php');
+            // exit;
+        
+        }
+    }
+
+    header("location: formulario.php");
+
+}
+
+/*Função de verificação de login*/ 
+
+function verificarLogin(){
+
+    if($_SESSION["id"] != session_id() || empty(session_id())){
+
+        header("location: formulario.php");
+
+    }
+
+}
+
+function finalizarLogin(){
+    session_unset(); //limpa todas as variáveis da sessão
+    session_destroy(); //destrói a sessão ativa
+
+    header("location: formulario.php");
+}
